4888de1d03515e6898692825ab8ed2ebd556c070
LeuleY <leulemerso2003@gmail.com>
2024-06-12 18:16:39 -0400

    Switch fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container, Grid } from "@mui/material";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Switch from '@mui/material/Switch';
+
+
+// import { Switch } from '@mui/material';
 
 
 
@@ -9,6 +14,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Resume from "./pages/Resume/Resume";
+
 
 
 import "./App.css";
