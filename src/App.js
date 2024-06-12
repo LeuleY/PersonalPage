25a920ed50bc4b2abf6a1877751cd7811d8d60f6
LeuleY <leulemerso2003@gmail.com>
2024-06-12 18:06:11 -0400

    Format and Installation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Grid } from "@mui/material";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
+
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Grid container>
+        <Grid item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          style={{ backgroundColor: "blue" }}
+          
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Profile />
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "red" }}>
+          <Header />
+
+          <Router>
+            <Switch>
+              <Route path="/Portfolio">
+                <Portfolio />
+              </Route>
+
+              <Route path="/">
+                <Resume />
+              </Route>
+            </Switch>
+          </Router>
+
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
