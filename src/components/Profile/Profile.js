af9b122ab0c8cbfee53504c59204bf2455d50be5
LeuleY <leulemerso2003@gmail.com>
2024-06-14 22:36:40 -0400

    Profile Section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,36 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+
+
+import "./Profile.css";
+
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile container_shadow'>
+      <div className='profile_name'>
+      <Typography classsName='name'>   Leule Yonas </Typography>
+      <Typography className= 'title'>  Software Engineer  </Typography>
+
+      </div>
+      
+        <figure className='profile_image'>   
+        
+            <img src={require('../../assets/images/pfp.jpg')} alt=""/>
+        </figure>
+
+        <div className='profile_information'>
+          Timeline
+          <br/>
+          
+          <button>
+            myButton
+          </button>
+
+
+        </div>
+      
+    </div>
   )
 }
 
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -9,3 +9,5 @@ const Resume = () => {
 }
 
 export default Resume
+
+
