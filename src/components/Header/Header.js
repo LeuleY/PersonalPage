d8cfa6d5c2328a8e35606fa8c40ee124d5619c5f
LeuleY <leulemerso2003@gmail.com>
2024-07-02 23:54:56 -0400

    NavBar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form, FormControl, Nav, Navbar, NavDropDown,} from "react-bootstrap";
+import {Link,NavLink, withRouter, useNavigate, useLocation, useParams} from "react-router-dom";
+import HomeRounded from '@mui/icons-material/HomeRounded';
+import SchoolRounded from '@mui/icons-material/SchoolRounded';
+import WorkRounded from '@mui/icons-material/WorkRounded';
+import Facebook from '@mui/icons-material/Facebook';
+import Twitter from '@mui/icons-material/Twitter';
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import GitHub from '@mui/icons-material/GitHub';
+import Telegram from '@mui/icons-material/Telegram';
 
 const Header = () => {
   return (
-    <div>
-        This is header
-    </div>
+    <Navbar expand = "lg" sticky = "top" className='header'>
+      <Nav.Link as ={NavLink} to = '/' >
+      <Navbar.Brand className='header_home'>
+        <HomeRounded />
+
+      </Navbar.Brand>
+      
+      </Nav.Link>
+
+    </Navbar>
   )
 }
 
