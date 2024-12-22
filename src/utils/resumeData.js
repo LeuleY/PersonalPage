eaf13c8e954466dcc39b6294f57573d891b4ac47
LeuleY <leulemerso2003@gmail.com>
2024-12-22 14:56:43 -0500

    Update to Resume Page

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -26,6 +26,16 @@ const resumeData = {
 
     Experiences: [
         {
+            Title: "Baltimore City Data Science/Knowledge Graph Researcher",
+            Date: "Sep 2024 - Dec 2024",
+            Description: "Conducted in-depth research on knowledge graph representations and evaluated Neo4j graph databases, increased readability by 45%. Successfully configured Neo4j graph database to support crime data analysis. Imported and queried real crime data using the Cypher query language."
+        },
+        {
+            Title: "Software Engineer Intern ( Market Research and Data Analyst) , Thematic",
+            Date: "Aug 2024 - Sep 2024",
+            Description: "Conducted detailed market research to identify and evaluate promising creators/creator communities to drive business growth. Crafted targeted top-of-funnel outreach messages to increase brand awareness and lead generation.Delivered strategic insights and actionable recommendations to develop innovative marketing strategies that fostered engagement and improved campaign efficiency."
+        },
+         {
             Title: "TU-COE Robotics Summer Camp Instructor",
             Date: "June 2024 - Aug 2024",
             Description: "TU-COE-Robotics Program focused on helping students improve STEM and creativity skills, and develop their expressive language."
@@ -53,7 +63,7 @@ const resumeData = {
     Skills: [
         {
             title: "LANGUAGES",
-            description: ["Java", "Python", "JavaScript", "R", "HTML", "CSS"]
+            description: ["Java", "Python", "JavaScript", "R", "HTML", "CSS" , "CYPHER"]
         },
         {
             title: "DBMS",
@@ -71,6 +81,48 @@ const resumeData = {
 
     projects: [
         {
+            tag: 'DataScience',
+            images: [
+                'https://i.imgur.com/ujb8gqG.png',
+                'https://i.imgur.com/rLVjBen.png',
+                'https://i.imgur.com/GJ5dL3w.png',
+                'https://i.imgur.com/Ae9XaDi.png',
+                
+                
+            ],
+            title: 'NEO4J Crime Graph Research',
+            caption: "Create and analyzing Baltimore Crime via Neo4J Graph Database.",
+            description: 'Conducted in-depth research on knowledge graph representations and evaluated Neo4j graph databases, increased readability by 45%. Successfully configured Neo4j graph database to support crime data analysis. Imported and queried real crime data using the Cypher query language.',
+            links: [
+                {
+                    link: 'https://311mappeddata.streamlit.app/',
+                    icon: <LaunchSharpIcon />
+                },
+                {
+                    link: 'https://github.com/LeuleY',
+                    icon: <GitHubIcon />
+                },
+            ],
+        },
+        {
+            tag: 'WebDev',
+            images: [
+                'https://i.imgur.com/JcHcdGY.png',
+                'https://i.imgur.com/lYJYdHr.png',
+                'https://i.imgur.com/lhOuWxE.png',
+                'https://i.imgur.com/X3VnpDj.png',
+            ],
+            title: 'RepRex',
+            caption: "Full Stack Workout Tracking Website.",
+            description: 'A service website that allows the user to track their workout. Includes PyPlot for visual tracking. Includes real time chat community page. Contributed in 30% code coverage.Used MongoDB Atlas to store user information and workout info , React for frontend . Constructed website code using Javascript, HTML and CSS. Used Vercel and Render to host websites.',
+            links: [
+                {
+                    link: 'https://github.com/LeuleY',
+                    icon: <GitHubIcon />
+                },
+            ],
+        },
+        {
             tag: 'WebDev',
             images: [
                 'https://i.imgur.com/Pj8YZ3B.png',
@@ -81,7 +133,7 @@ const resumeData = {
             description: 'A service website that allows the user to enter an artist of their choice. The website will then display the top 10 songs the artist has in order of highest streams. Used Python to construct the code and receive client ID and client Secret from Spotify API. Used Flask to translate HTML code to display to live host.',
             links: [
                 {
-                    link: 'www.google.com',
+                    link: 'https://github.com/LeuleY',
                     icon: <GitHubIcon />
                 },
             ],
@@ -95,7 +147,7 @@ const resumeData = {
                 'https://i.imgur.com/kLovFOD.png',
                 'https://i.imgur.com/MHKrtSG.png',
             ],
-            title: 'Baltimore Data Science Corps',
+            title: 'Baltimore Data Science Corps(NSF)',
             caption: "Research Program with the NSF analyzing Baltimore crime and 311 calls to track trends.",
             description: 'Baltimore Data Science Corps focused on analytics of Baltimore 311 calls and Baltimore crime. Researched, analyzed, and organized public Baltimore crime data to highlight and bring awareness to criminal activity problems. Used Streamlit and Pandas with Python to clean the data and transform CSV files into a functional map graph to visualize the cleaned data. Worked in an Agile environment to organize data and look at trends affecting the city of Baltimore.',
             links: [
